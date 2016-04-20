de6d4de7fa1fde8098c4b058ad831968d9fb66af
redlegoman <github.com@mcdade.co.uk>
2016-04-20 14:12:11 +0100

    changed url to rawgit.com

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ Pebble.addEventListener('ready', function() {
 });
 
 Pebble.addEventListener('showConfiguration', function() {
-  var url = 'https://mcdade.co.uk/pebble/index.html';
+  var url = 'https://rawgit.com/redlegoman/Colour-RLM-Simple-Watch-w-Config/master/web/index.html';
   console.log('Showing configuration page: ' + url);
 
   Pebble.openURL(url);
